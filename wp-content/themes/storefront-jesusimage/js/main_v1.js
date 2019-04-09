ae0747bcc5216631f48f4b407d86efe363c7c116
Yoann Gern <yoann@yoanngern.ch>
2019-04-09 10:14:42 -0400

    update mobile nav scroll

--- wp-content/themes/storefront-jesusimage/js/main_v1.js
+++ wp-content/themes/storefront-jesusimage/js/main_v1.js
@@ -1,6 +1,7 @@
 // @codekit-prepend "vendor/jquery-2.2.2.js"
 // @codekit-append "vendor/jquery.slides.js"
 
+var isMobile = false;
 
 $(document).ready(function () {
 
@@ -8,29 +9,57 @@ $(document).ready(function () {
 
         $('body').addClass("mobile");
 
+        isMobile = true;
     }
 
-    $('#burger').click(function () {
+    $('body > header > .global li.menu-item-has-children > a').on('click', function (e) {
 
-        $('body header#masthead .global').toggleClass('open_nav');
+
+        if (!$(this).hasClass('click-on') && isMobile) {
+            e.preventDefault();
+        }
 
     });
 
 
-    $('body header#masthead > .global ul.menu li').each(function () {
+    $('#burger').click(function () {
+
+        $('body > header > .global').toggleClass('open_nav');
+    });
+
+
+    $('body > header > .global ul.menu > li').each(function () {
 
         $('ul.sub-menu', this).addClass('hidden');
 
+
     }).mouseenter(function () {
 
-        $('ul.sub-menu', this).addClass('active');
+        if (!isMobile) {
+            $('ul.sub-menu', this).addClass('active');
 
-        $('ul.sub-menu', this).removeClass('hidden');
+            $('ul.sub-menu', this).removeClass('hidden');
+        }
 
     }).mouseleave(function () {
+        if (!isMobile) {
+            $('ul.sub-menu', this).removeClass('active').addClass('hidden');
+        }
 
-        $('ul.sub-menu', this).removeClass('active').addClass('hidden');
+    }).on('click', function () {
+        if (isMobile) {
 
+
+            $('ul.sub-menu', this).addClass('active');
+
+            $('body > header > .global ul.menu > li').each(function () {
+                $('ul.sub-menu', this).addClass('hidden');
+                $('> a', this).removeClass('click-on');
+            });
+
+            $('ul.sub-menu', this).removeClass('hidden');
+            $('> a', this).addClass('click-on');
+        }
     });
 
 });
