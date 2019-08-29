19df3e8614640c11d9a6e358d824843cab061fbf
Yoann Gern <yoann@yoanngern.ch>
2019-08-29 15:35:02 -0400

    hurricane message

---
+++ wp-content/themes/storefront-jesusimage/hurricane.php
@@ -0,0 +1,10 @@
+<style>
+<?php
+
+include 'css/hurricane.css'; ?>
+</style>
+
+
+<section id="hurricane">
+    <p>Due to Hurricane Dorian, our offices are closed through September 4th. <a href="https://jesusimage.tv/hurricane">More Info</a></p>
+</section>
